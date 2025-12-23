907b9a69889a13bae4bd4e314eaecbbd38620559
heyjinjung <heyjinjung@users.noreply.github.com>
2025-12-23 17:02:40 +0900

    기능 개선: 앱 제목을 '씨씨 신규금고서비스'로 설정하고 Head 컴포넌트를 추가하여 메타데이터 관리

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,5 +1,13 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>씨씨 신규금고서비스</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
